40652943587ae1ac6b698fca1c0a4a0d0b14de24
zayrdev <bossleboss295@gmail.com>
2026-05-28 16:42:47 +0200

    Fix checkout URL for Render

--- cart.js
+++ cart.js
@@ -2,7 +2,7 @@
 
 async function checkoutStripe(itemsToCheckout, discountCode = null) {
   try {
-    const response = await fetch('http://localhost:3000/create-checkout-session', {
+    const response = await fetch('/create-checkout-session', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ items: itemsToCheckout, discountCode })
